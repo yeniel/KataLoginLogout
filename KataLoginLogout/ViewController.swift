9d729e8dc0363b5af07dcf8f65f4ed7c76c330c1
Yeniel Landestoy <y3ni3l@gmail.com>
2018-09-20 16:02:11 +0200

    new featrue admin admin

--- KataLoginLogout/ViewController.swift
+++ KataLoginLogout/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userTextEdit: UITextField!
+    @IBOutlet weak var passwordTextEdit: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginButtonTap(_ sender: Any) {
+        if userTextEdit.text == "admin"
+            && passwordTextEdit.text == "admin"
+        {
+            let alert = UIAlertController(title: "Login Successfully",
+                                          message: "",
+                                          preferredStyle: .alert)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
 
