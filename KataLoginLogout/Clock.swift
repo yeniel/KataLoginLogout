28ba88231636836787b5c312bdd68278dca780aa
Yeniel Landestoy <y3ni3l@gmail.com>
2018-09-20 16:55:09 +0200

    tests date

---
+++ KataLoginLogout/Clock.swift
@@ -0,0 +1,9 @@
+//
+//  Clock.swift
+//  KataLoginLogout
+//
+//  Created by Yeniel Landestoy on 20/9/18.
+//  Copyright © 2018 Yeniel Landestoy. All rights reserved.
+//
+
+import Foundation
---
+++ KataLoginLogout/KataApp.swift
@@ -0,0 +1,9 @@
+//
+//  KataApp.swift
+//  KataLoginLogout
+//
+//  Created by Yeniel Landestoy on 20/9/18.
+//  Copyright © 2018 Yeniel Landestoy. All rights reserved.
+//
+
+import Foundation
