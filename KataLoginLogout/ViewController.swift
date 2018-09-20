e78b9f924bf905715a3ef7bfa21d6db0896d58c7
Yeniel Landestoy <y3ni3l@gmail.com>
2018-09-20 15:36:54 +0200

    Initial Commit

---
+++ KataLoginLogout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KataLoginLogout
+//
+//  Created by Yeniel Landestoy on 20/9/18.
+//  Copyright © 2018 Yeniel Landestoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KataLoginLogoutTests/KataLoginLogoutTests.swift
@@ -0,0 +1,36 @@
+//
+//  KataLoginLogoutTests.swift
+//  KataLoginLogoutTests
+//
+//  Created by Yeniel Landestoy on 20/9/18.
+//  Copyright © 2018 Yeniel Landestoy. All rights reserved.
+//
+
+import XCTest
+@testable import KataLoginLogout
+
+class KataLoginLogoutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
